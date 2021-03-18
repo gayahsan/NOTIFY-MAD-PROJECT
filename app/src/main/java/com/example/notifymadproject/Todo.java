58b4d1bd14b146750280e82ab5c3df3a31740e85
Anonymous <sasiru1heshan@gmail.com>
2021-03-18 17:20:34 +0530

    heshan commit

---
+++ app/src/main/java/com/example/notifymadproject/Todo.java
@@ -0,0 +1,14 @@
+package com.example.notifymadproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Todo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo);
+    }
+}
