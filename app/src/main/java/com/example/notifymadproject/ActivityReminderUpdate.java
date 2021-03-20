768966163e19c2069e3f05cc273e8765f4e0ef49
raviK123456 <ravindukaushan1@gmail.com>
2021-03-20 22:34:24 +0530

    Commit 03 Ravindu Reminder Create & Update Pages

---
+++ app/src/main/java/com/example/notifymadproject/ActivityReminderUpdate.java
@@ -0,0 +1,14 @@
+package com.example.notifymadproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityReminderUpdate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reminder_update);
+    }
+}
