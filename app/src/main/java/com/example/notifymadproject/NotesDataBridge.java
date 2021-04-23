ae0cf27a3752e8c2b44c41afef9d4a759c122a9e
witchx44 <witchx44@gmail.com>
2021-04-24 00:25:05 +0530

    Notepad Backend Init.

---
+++ app/src/main/java/com/example/notifymadproject/NotesDataBridge.java
@@ -0,0 +1,63 @@
+package com.example.notifymadproject;
+
+public class NotesDataBridge { // This class works as a servlet in dynamic web projects
+    long id;
+    private String title, content, date, time;
+
+    NotesDataBridge(){}
+
+    NotesDataBridge(String title, String content, String date, String time) {
+        this.title = title;
+        this.content = content;
+        this.date = date;
+        this.time = time;
+    }
+
+    NotesDataBridge(long id, String title, String content, String date, String time) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.date = date;
+        this.time = time;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
