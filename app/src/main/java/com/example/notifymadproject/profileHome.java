4d32b5f08bb5be5232dc8e7533570414fa0f06f4
Nawagamuwa.N.G.K <gayashankavinga@gmail.com>
2021-04-29 16:06:40 +0530

    back end 1st comit

--- app/src/main/java/com/example/notifymadproject/profileHome.java
+++ app/src/main/java/com/example/notifymadproject/profileHome.java
@@ -1,14 +1,109 @@
 package com.example.notifymadproject;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
 
+import android.content.ContentUris;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class profileHome extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class profileHome extends AppCompatActivity  implements LoaderManager.LoaderCallbacks <Cursor> {
+
+    public ProfileAdapter mAdapter;
+    public static final  int CONTACTLOADER = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_home);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(profileHome.this, profileEdit.class);
+                startActivity(intent);
+            }
+        });
+
+        ListView listView = findViewById(R.id.list);
+        mAdapter = new ProfileAdapter(this, null);
+        listView.setAdapter(mAdapter);
+
+        // whenever we press a listview for updating
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(profileHome.this, profileEdit.class);
+                Uri newUri = ContentUris.withAppendedId(ProfileContract.ContactEntry.CONTENT_URI, id);
+                intent.setData(newUri);
+                startActivity(intent);
+
+            }
+        });
+
+        // get the loader running
+        //getLoaderManager().initLoader(CONTACTLOADER, null, this);
     }
+
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id,  Bundle args)  {
+
+        String[] projection = {ProfileContract.ContactEntry._ID,
+                ProfileContract.ContactEntry.COLUMN_NAME,
+                ProfileContract.ContactEntry.COLUMN_EMAIL,
+                ProfileContract.ContactEntry.COLUMN_PICTURE,
+                ProfileContract.ContactEntry.COLUMN_PHONENUMBER,
+                ProfileContract.ContactEntry.COLUMN_TYPEOFCONTACT
+        };
+
+//        return new CursorLoader(this, ProfileContract.ContactEntry.CONTENT_URI,
+//                projection, null,
+//                null,
+//                null);
+        return null;
+    }
+
+
+//    @Override
+//    public Loader<Cursor> onCreateLoader(int id,  Bundle args) {
+//
+//        String[] projection = {ProfileContract.ContactEntry._ID,
+//                ProfileContract.ContactEntry.COLUMN_NAME,
+//                ProfileContract.ContactEntry.COLUMN_EMAIL,
+//                ProfileContract.ContactEntry.COLUMN_PICTURE,
+//                ProfileContract.ContactEntry.COLUMN_PHONENUMBER,
+//                ProfileContract.ContactEntry.COLUMN_TYPEOFCONTACT
+//        };
+//        return new CursorLoader(this, ProfileContract.ContactEntry.CONTENT_URI,
+//                projection null,
+//                null,
+//                null);
+//    }
+
+    @Override
+    public void onLoadFinished( androidx.loader.content.Loader<Cursor> loader, Cursor data) {
+        mAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset( androidx.loader.content.Loader<Cursor> loader) {
+        mAdapter.swapCursor(null);
+    }
+
+
+
 }
