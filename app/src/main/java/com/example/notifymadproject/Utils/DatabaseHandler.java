6290480172fe00626a61b54b999e7e65c8336f8c
Anonymous <sasiru1heshan@gmail.com>
2021-05-09 20:55:39 +0530

    Merge master

--- app/src/main/java/com/example/notifymadproject/Utils/DatabaseHandler.java
+++ app/src/main/java/com/example/notifymadproject/Utils/DatabaseHandler.java
@@ -10,7 +10,7 @@ import com.example.notifymadproject.Model.ToDoModel;
 
 import java.util.ArrayList;
 import java.util.List;
-
+//this is TodoListDataBase
 public class DatabaseHandler extends SQLiteOpenHelper {
 
     private static final int VERSION = 1;
